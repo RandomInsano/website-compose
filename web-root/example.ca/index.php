d77952939df9ab73662abc6ff21e351cdad98144
Edwin Amsler <edwinguy@gmail.com>
2020-02-16 12:24:49 -0600

    Provide demo PHP script in the webroot

---
+++ web-root/example.ca/index.php
@@ -0,0 +1,7 @@
+<?php $value = "example.ca"; ?> 
+<html>
+  <body>
+    <h1>Hello from <?= $value ?>!</h1>
+  </body>
+</html>
+
---
+++ web-root/example.com/index.php
@@ -0,0 +1,7 @@
+<?php $value = "example.com"; ?> 
+<html>
+  <body>
+    <h1>Hello from <?= $value ?>!</h1>
+  </body>
+</html>
+
