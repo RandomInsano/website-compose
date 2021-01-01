a94acbb8b8a15dcff66c34ae3d913b8f4b89adff
Edwin Amsler <EdwinGuy@GMail.com>
2021-01-01 12:27:41 -0600

    Scrub out example website, remove Apache Bench
    Since URLs are going to change, removeing `ab` from the script. I can add it back in once I've researched how to unpack data from flags in bash scripts.

--- upgrade.sh
+++ upgrade.sh
@@ -8,9 +8,9 @@ if [ $# -eq 0 ]; then
 	echo "Usage: $0 [-dpr]"
 	echo ""
 	echo "Options:"
-	echo " -d	Delete unused images (from all of docker!)"
-	echo " -p	Pull updates from dockerhub "
-	echo " -r	Rebuild deployment"
+	echo " -d		Delete unused images (from all of docker!)"
+	echo " -p		Pull updates from dockerhub "
+	echo " -r		Rebuild deployment"
 	echo ""
 	exit 1
 fi
@@ -22,10 +22,6 @@ fi
 if [[ $* == *-r* ]]; then
 	docker-compose down
 	docker-compose up -d
-
-	echo "Waiting ten seconds to settle before benchmarking..."
-	sleep 10
-	ab -n 1000 -c 20 https://torrentialequilibrium.net/index.php
 fi
 
 if [[ $* == *-d* ]]; then
